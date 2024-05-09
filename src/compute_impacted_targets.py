11ee5b08bee99a76043f4b7842fb42965b66009b
Phil Vendola <pv72895@gmail.com>
2024-05-09 22:02:46 +0000

    Another pass at generating impacted targets

--- src/compute_impacted_targets.py
+++ src/compute_impacted_targets.py
@@ -10,9 +10,10 @@ def log_if_verbose(log):
         print(log)
 
 nx_workspace_path = os.environ.get("WORKSPACE_PATH")
-if not nx_workspace_path:
-    print("No Nx workspace path found. Exiting.")
-    sys.exit(1)
+if nx_workspace_path:
+    run_command(f"cd {nx_workspace_path}")
+
+run_command("npm install --force")
 
 merge_instance_branch = os.environ.get("MERGE_INSTANCE_BRANCH")
 if not merge_instance_branch:
@@ -33,19 +34,21 @@ affected_json_out=f"./{merge_instance_branch_head_sha}_{pr_branch_head_sha}.json
 
 run_command(f"npx nx graph --base={merge_instance_branch_head_sha} --head={pr_branch_head_sha} --file={affected_json_out}", verbose=verbose)
 
-run_command(f"cat {affected_json_out}")
+affected_json = run_command(f"cat {affected_json_out}", return_output=True)
+print(affected_json)
 
 # Output Files
 impacted_projects_out=f"./impacted_projects_{pr_branch_head_sha}"
 run_command(f"cmd '{affected_json_out}' | jq -r '.affectedProjects[] | select(length > 0)' >> '${impacted_projects_out}'")
 
-num_impacted_projects=run_command(f"wc -l < '{impacted_projects_out}'")
+num_impacted_projects=run_command(f"wc -l < '{impacted_projects_out}'", return_output=True)
 print(f"Computed {num_impacted_projects} projects for sha {pr_branch_head_sha}")
 
 print("To replicate this command, run the following")
 print(f"npx nx graph --verbose --affected --base='{merge_instance_branch_head_sha}' --head='{pr_branch_head_sha}' --file='${affected_json_out}'")
 
-run_command("cat '${impacted_projects_out}'")
+impacted_projects = run_command("cat '${impacted_projects_out}'", return_output=True)
+print(impacted_projects)
 
 # Outputs
 github_output = f"impacted_projects_out={impacted_projects_out}\n"
@@ -53,4 +56,3 @@ log_if_verbose(f"Setting these outputs:\n{github_output}")
 
 with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
     f.write(github_output)
-    
